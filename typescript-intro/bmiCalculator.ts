d38c64bf92186853b7a91c7044fc032be5bf4b83
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-10-31 19:10:04 +0200

    Add express and a simple HTTP GET endpoint
    - Update tsconfig.json file
    - Fix occured errors with bmiCalculator
    - Exercise 9.4

--- typescript-intro/bmiCalculator.ts
+++ typescript-intro/bmiCalculator.ts
@@ -4,24 +4,27 @@ interface BmiParameters {
 }
 
 const parseArguments = (args: string[]): BmiParameters => {
-  if (args.length < 4) throw new Error("Not enough arguments")
-  if (args.length > 4) throw new Error("Too many arguments")
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
 
   const height = Number(args[2])
   const weight = Number(args[3])
 
   if (isNaN(height) || isNaN(weight))
-    throw new Error("Arguments must be numbers!")
+    throw new Error('Arguments must be numbers!')
 
   return { height, weight }
 }
 
 const calculateBmi = (height: number, weight: number): string => {
+  if (height <= 0 || weight <= 0)
+    throw new Error("Arguments can't be negative or zero!")
   const bmi = (weight / height ** 2) * 10_000
 
-  if (bmi >= 18.5 && bmi <= 24.9) return "Normal (healthy weight)"
-  if (bmi < 18.5) return "Underweight"
-  if (bmi > 24.9) return "Overweight"
+  if (bmi >= 18.5 && bmi <= 24.9) return 'Normal (healthy weight)'
+  if (bmi < 18.5) return 'Underweight'
+  if (bmi > 24.9) return 'Overweight'
+  return 'Something went wrong'
 }
 
 try {
