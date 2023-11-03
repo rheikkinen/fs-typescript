0d8d8933cdd0c7d0936548755b7f717e14171902
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-11-03 13:29:45 +0200

    Add cors to make backend work with frontend
    - frontend: https://github.com/rheikkinen/fs-patientor-frontend
    - Exercise 9.9

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 const PORT = 3000;
 
